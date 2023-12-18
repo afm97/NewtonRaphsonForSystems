c0749317269cfd7e6ad46e4b92a593d26bf8c7ca
afm97 <arthurfranca97@gmail.com>
2023-12-18 00:37:01 -0300

    Finalizacao da implementacao das derivadas

--- derivatives.h
+++ derivatives.h
@@ -1,26 +1,16 @@
+typedef double (*function)(double, double, double);
 
-typedef double (*function)(double, double, double, double);
-
-double derivative_function_x1(double x_1, double x_2, double x_3, double x_4, double h, function f)
+double derivative_function_x1(double x_1, double x_2, double x_3, double h, function f)
 {
-
-    return ((f(x_1 + h, x_2, x_3, x_4) - f(x_1, x_2, x_3, x_4)) / h);
+    return (f(x_1 + h, x_2, x_3) - f(x_1, x_2, x_3)) / h;
 }
 
-double derivative_function_x2(double x_1, double x_2, double x_3, double x_4, double h, function f)
+double derivative_function_x2(double x_1, double x_2, double x_3, double h, function f)
 {
-
-    return ((f(x_1, x_2 + h, x_3, x_4) - f(x_1, x_2, x_3, x_4)) / h);
+    return (f(x_1, x_2 + h, x_3) - f(x_1, x_2, x_3)) / h;
 }
 
-double derivative_function_x3(double x_1, double x_2, double x_3, double x_4, double h, function f)
+double derivative_function_x3(double x_1, double x_2, double x_3, double h, function f)
 {
-
-    return ((f(x_1 + h, x_2, x_3 + h, x_4) - f(x_1, x_2, x_3, x_4)) / h);
-}
-
-double derivative_function_x4(double x_1, double x_2, double x_3, double x_4, double h, function f)
-{
-
-    return ((f(x_1 + h, x_2, x_3, x_4 + h) - f(x_1, x_2, x_3, x_4)) / h);
+    return (f(x_1, x_2, x_3 + h) - f(x_1, x_2, x_3)) / h;
 }
