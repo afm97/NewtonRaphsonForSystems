2223698f4d4457674615d797d46a4ac1b6cc697e
afm97 <arthurfranca97@gmail.com>
2023-12-18 01:23:18 -0300

    Agora as funcoes suportam equacoes de quatro variaveis

--- newtonRaphson.h
+++ newtonRaphson.h
@@ -13,11 +13,12 @@ void NewtonRaphson(unsigned int numberOfRows,
     {
         counter++;
 
-        jacobian(initialValues[0], initialValues[1], initialValues[2], h, matrix);
+        jacobian(initialValues[0], initialValues[1], initialValues[2], initialValues[3], h, matrix);
 
-        independentValues[0] = -func1(initialValues[0], initialValues[1], initialValues[2]);
-        independentValues[1] = -func2(initialValues[0], initialValues[1], initialValues[2]);
-        independentValues[2] = -func3(initialValues[0], initialValues[1], initialValues[2]);
+        independentValues[0] = -func1(initialValues[0], initialValues[1], initialValues[2], initialValues[3]);
+        independentValues[1] = -func2(initialValues[0], initialValues[1], initialValues[2], initialValues[3]);
+        independentValues[2] = -func3(initialValues[0], initialValues[1], initialValues[2], initialValues[3]);
+        independentValues[3] = -func4(initialValues[0], initialValues[1], initialValues[2], initialValues[3]);
 
         gaussianElimination(numberOfRows, matrix, independentValues);
 
@@ -30,5 +31,5 @@ void NewtonRaphson(unsigned int numberOfRows,
         printArray(numberOfRows, initialValues);
         transferValuesOfArrays(numberOfRows, independentValues, initialValues);
 
-    } while (normOfArray > tolerance && counter < numberMaxOfIterations);
+    } while (normOfArray > tolerance && counter <= numberMaxOfIterations);
 }
